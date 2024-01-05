cb2ed632e5a70720c1c518b42d4757e03c7acc22
Ercode Techoglu <ercode@pm.me>
2024-01-05 09:11:19 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const addition = (a: number, b: number) => {
+  // Typescript will guess the function returns a number
+  return a + b;
+};
+
+const number1 = 5;
+const number2 = 10;
+const result = addition(number1, number2); // Result will be a number
+
+console.log("The result is %d", result);
